52bd583cb0b94787cb68c4a12e5699d36edc860a
Natalia Pankevich <pankevicnatalia4@gmail.com>
2023-05-02 21:34:23 +0300

    commit

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('.hero__btn');
+const modal = document.querySelector('.modal');
+
+
+btn.addEventListener('click', ()=>{
+    modal.classList.add('modal__open');
+});
+
+modal.addEventListener('click', (event)=>{
+    if (event.target === modal || event.target.closest('.modal__close')) {
+        modal.classList.remove('modal__open');        
+    }  
+})
